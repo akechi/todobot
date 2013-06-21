86b4af673eb90676e21df4120961bf57a1ffb4a5
bgnori <bgnori@gmail.com>
2013-06-21 23:21:24 +0900

    unittests! 	modified:   Makefile 	modified:   test_todo.py

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 
 
-help:
-	python todo_read.py tests/help-in.json help-out.txt
 
-add:
-	python todo_read.py tests/add-in.json add-out.txt
+test:
+	python test_todo.py 
+
+help:
+	python todo.py tests/help-in.json help-out.txt
+
--- test_todo.py
+++ test_todo.py
@@ -20,6 +20,102 @@ class ToDoBotTestCase(unittest.TestCase):
         xs = v.splitlines()
         self.assertTrue('#todo show [id]' in xs)
 
+    @unittest.skip('no database schema')
+    def test_add(self):
+        req = """{"events":[{"message":{"text":"#todo add test_add","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_addto(self):
+        req = """{"events":[{"message":{"text":"#todo addto bgnori test_addto","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_list_all(self):
+        req = """{"events":[{"message":{"text":"#todo list-all","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_list_done(self):
+        req = """{"events":[{"message":{"text":"#todo list-done","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_list(self):
+        req = """{"events":[{"message":{"text":"#todo list","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+
+    @unittest.skip('no database schema')
+    def test_listof_all(self):
+        req = """{"events":[{"message":{"text":"#todo listof-all","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_listof_done(self):
+        req = """{"events":[{"message":{"text":"#todo listof-done","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_listof(self):
+        req = """{"events":[{"message":{"text":"#todo listof bgnori","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_list_everything(self):
+        req = """{"events":[{"message":{"text":"#todo list-everything","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_done(self):
+        req = """{"events":[{"message":{"text":"#todo done 1","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_del(self):
+        req = """{"events":[{"message":{"text":"#todo del 2","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
+
+    @unittest.skip('no database schema')
+    def test_show(self):
+        req = """{"events":[{"message":{"text":"#todo show 3","speaker_id":"raa0121","room":"computer_science"}}]}"""
+        self.bot.serve_as_cgi(len(req))
+        v = sys.stdout.getvalue()
+        self.assertTrue(v.startswith('Content-type: text/html\n'))
+        ''' need some assertions '''
 
 if __name__ == '__main__':
     unittest.main()
