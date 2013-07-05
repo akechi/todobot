d059e14c2b311b586e9a897459f9c5c00de818ff
Noriyuki Hosaka <nori@isokaze.(none)>
2013-07-05 22:08:05 +0900

    new distrubte ver with $DISTRIBUTE_VER no distribute in freeze.txt 	modified:   freeze.txt 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+DISTRIBTE_VER=0.6.49
 ./pyenv-3.3.py py3.3
 source py3.3/bin/activate
-curl -O "http://pypi.python.org/packages/source/d/distribute/distribute-0.6.45.tar.gz"
-tar xf distribute-0.6.45.tar.gz
-cd distribute-0.6.45
-python setup.py install
+curl -l -O "https://pypi.python.org/packages/source/d/distribute/distribute-${DISTRIBTE_VER}.tar.gz"
+tar xf distribute-${DISTRIBTE_VER}.tar.gz
+cd distribute-${DISTRIBTE_VER}
+../py3.3/bin/python setup.py install
 cd ..
 ./py3.3/bin/easy_install-3.3 pip
 
